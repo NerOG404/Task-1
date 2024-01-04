6dcd7048a51a3847c7db2292b7256cd7861e5984
NerOG404 <mohamadsaefudin042@gmail.com>
2024-01-04 19:26:45 +0700

    Update 4 post blog

--- assets/js/script-form.js
+++ assets/js/script-form.js
@@ -1,17 +1,19 @@
 function submitData() {
-const userName = document.getElementById('name').value;
-const email = document.getElementById('email').value;
-const age = document.getElementById('age').value;
-const role = document.getElementById('role').value;
-const feature = document.getElementById('feature').value;
-const area = document.getElementById('area').value;
-const radio = document.querySelector('input[name="radio"]:checked').value;
-const getChecked = function () {
-    const checkboxes = document.querySelectorAll('input[type=checkbox]:checked');
-    const values = [];
-    for (let i = 0; i < checkboxes.length; i++) {
-    values.push(checkboxes[i].value);
-    }
+    const userName = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const age = document.getElementById('age').value;
+    const role = document.getElementById('role').value;
+    const feature = document.getElementById('feature').value;
+    const area = document.getElementById('area').value;
+    const radio = document.querySelector('input[name="radio"]:checked').value;
+
+    const getChecked = function () {
+        const checkboxes = document.querySelectorAll('input[type=checkbox]:checked');
+            const values = [];
+            for (let i = 0; i < checkboxes.length; i++) {
+            values.push(checkboxes[i].value);
+        }
+
     return values;
 };
 
@@ -34,8 +36,8 @@ if (userName !== '' && email !== '' && age !== '' && role !== '' && feature !== 
         console.log(`${key}: ${data[key]}`)};
 
     return alert(`Nama : ${userName}\nEmail : ${email}\nAge : ${age}\nYour current role : ${role}\nWould you recommend MyWeb to a friend : ${radio}\nWhat is your favorite feature : ${feature}\nWhat would you like to see improved : ${getChecked()}\nAny comments : ${area}`);
+    
 } else if (userName === '' || email === '' || age === '' || role === '' || feature === '' || area === '' || radio === '') {
     return alert('All input fields must be not empty');
-}
-}
+}}
 
